ee88cbb69f11ff9b3a848cd65edd7cbcd9d66a60
Dicky Arya Kesuma <dickyaryakesuma@gmail.com>
2019-11-21 03:08:51 +0700

    Penambahan .env

--- driver/driver.go
+++ driver/driver.go
@@ -2,13 +2,34 @@ package driver
 
 import (
 	"database/sql"
+	"log"
+	"os"
+
 	//driver mysql
+	. "fmt"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 //Connect : Fungsi Untuk Konek Database
 func Connect() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:mysql@tcp(127.0.0.1:3306)/halovet_db")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbUSERNAME := os.Getenv("DB_USERNAME")
+	dbNAME := os.Getenv("DB_NAME")
+	dbPASSWORD := os.Getenv("DB_PASSWORD")
+
+	dbURI := Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s",
+		dbUSERNAME,
+		dbPASSWORD,
+		dbNAME,
+	)
+
+	db, err := sql.Open("mysql", dbURI)
 	if err != nil {
 		return nil, err
 	}
